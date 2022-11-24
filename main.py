08c15500ba52ada77ca4870f48f95ace5e5f3c02
Sarah Silva <81635395+SarahMaressa@users.noreply.github.com>
2022-11-24 01:16:42 +0000

    Consulta a banco

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Importar arquivo Pessoa.py no diretório model
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 # Exemplo de uso
 poyatos = Pessoa(1, "Henrique Poyatos")
@@ -7,3 +9,28 @@ print(poyatos)
 
 # Mostrar só o nome
 print(poyatos.nome)
+print("\nAcesso ao banco...\n")
+
+# Chamar objeto de banco de dados
+db = Database()
+
+# Instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+# Carregar uma lista com o que veio do banco de dados
+pessoas = pessoaDAO.getAll(orderBy = True)
+for pessoa in pessoas:
+  print(pessoa)
+
+
+# Criar um objeto com qualquer ator/atriz/diretor
+novo = Pessoa(0, "Denzel Washington")
+
+novo = pessoaDAO.save(novo)
+
+print(" ")
+
+# Consultar banco novamente
+pessoas = pessoaDAO.getAll(orderBy = True)
+for pessoa in pessoas:
+  print(pessoa)
